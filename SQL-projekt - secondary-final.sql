25d074bb967a2e4982dc160e451b63e3e146159c
Adéla Havlová <103034819+HavlovaAdela@users.noreply.github.com>
2023-11-29 21:35:04 +0100

    Update SQL-projekt - secondary-final.sql
    Sjednoceno formátování

--- SQL-projekt - secondary-final.sql
+++ SQL-projekt - secondary-final.sql
@@ -7,8 +7,8 @@ SELECT
 	c.population
 FROM economies e
 LEFT JOIN countries c
-	ON e.country=c.country
-WHERE c.continent= 'Europe' AND e.YEAR >2005 AND e.YEAR<2020;
+	ON e.country = c.country
+WHERE c.continent = 'Europe' AND e.YEAR > 2005 AND e.YEAR < 2020 ;
 
 
 CREATE TABLE t_adela_havlova_sql_project_secondary_final AS
@@ -18,13 +18,12 @@ SELECT
 	pez.GDP,
 	pez.gini,
 	pez.YEAR,
-	Round((pez2.gini*100/pez.gini)-100,2)AS YEAR_on_year_gini_diff,
-	Round((pez2.gdp*100/pez.gdp)-100,2)AS YEAR_on_year_gdp_diff
+	ROUND ((pez2.gini * 100 / pez.gini) - 100, 2)AS YEAR_on_year_gini_diff,
+	ROUND ((pez2.gdp * 100 / pez.gdp) - 100, 2)AS YEAR_on_year_gdp_diff
 FROM t_adela_havlova_pomocna_evropske_zeme pez
 JOIN t_adela_havlova_pomocna_evropske_zeme pez2
-	ON pez2.YEAR= pez.YEAR +1
-	AND pez.country=pez2.country;
-
+	ON pez2.YEAR = pez.YEAR + 1
+	AND pez.country = pez2.country ;
 
 -- 5. Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, 
 -- projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
@@ -36,14 +35,15 @@ SELECT
 	sf.YEAR,
 	sf.YEAR_on_year_gdp_diff,
 	pf.YEAR AS 'year2',
-	pf.`year-on_year_difference_average_payroll` ,
+	pf.`year-on_year_difference_average_payroll`,
 	pf.year_on_year_diff_average_food 
 FROM t_adela_havlova_sql_project_secondary_final sf
 JOIN t_adela_havlova_sql_projekt_primary_final pf
-	ON sf.YEAR= pf.YEAR;
+	ON sf.YEAR = pf.YEAR ;
 
 SELECT * FROM t_adela_havlova_sjednoceni_primary_secondary tahsps
 WHERE country='czech republic'
 GROUP BY `YEAR` ;
 
+	
 	
